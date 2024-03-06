13a7938503b2192420913c75cae9e80ce130ddff
Víctor Miracle Macotela <victor.miracle@uabc.edu.mx>
2024-03-05 18:13:39 -0600

    Agregué cinco componentes con contenido mínimo, uno para inicio de sesión (login) y cuatro que incluirán las funciones especificadas en los requerimientos del ejercicio, pero que de momento solo tienen un mensaje para distinguir entre ellos.
    Las funciones de enrutamiento y lazy loading ya se encuentran implementadas.

--- src/app/app-routing.routes.ts
+++ src/app/app-routing.routes.ts
@@ -1,5 +1,24 @@
 import { Routes } from '@angular/router'
 
-export const routes: Routes = [
+import { LoginComponent } from './login/login.component'
 
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'app',
+    loadChildren: () => import('./features/features.module').then(m => m.FeaturesModule)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
 ]
---
+++ src/app/features/date-calc/date-calc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'date-calc',
+  templateUrl: 'date-calc.component.html',
+  styleUrls: ['date-calc.component.css']
+})
+export class DateCalcComponent {
+
+}
